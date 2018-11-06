c2caeeda048516f9e2251c649645bbef35e8a605
jack eichner <jack8191@gmail.com>
2018-11-06 15:49:06 -0800

    made api calls and quote display to dom work correctly. Image display still broken.

--- script.js
+++ script.js
@@ -1,78 +1,48 @@
-//we need a function each to access data from Getty Images and Genius api's
-//we need a function to create the html from the results
-//we need a function to render the html to the DOM
-//we need a function to handle the form submission
 
-//function accessUnsplashApi(){
-
-//}
-
-//function accessGeniusApi(){
-
-//}
-
-//function createResult(){
-
-//}
-
-//function renderResult(){
-
-//}
-
-//function watchSubmit(){
-
-//}
-
-// user submits a search
-// use the search to request Getty Images and display
-  // show even if the other fails
-// use the search to request Genius api's and display
-  // show even if the other fails
-
-// do both searches in concurrently
 function searchUnsplashImages(searchTerm, callback) {
-    let url = 'https://api.unsplash.com/random/';
-    let params = {
-        client_id: '9d5c5f49501e8179e3c0fcd3a9e88e155c30a4084ee5141799c20d0db8991db5',
-        query: `${searchTerm}`
-    };
-    $.getJSON(url, params, callback);
-    }
-    
-    function displayUnspashImages(responseData) {
-        let displayImage = `
-        <div class="js-search-results>
-            <img src=${responseData.urls.full} alt='a random unsplash image'>
-        `;
-        $('.js-search-results').html(displayImage);
-        
-    }
-
-    function getRandomQuote(callback){
-      let url = 'https://talaikis.com/api/quotes/random/';
-      $.getJSON(url, callback)
-    }
-
-    function displayRandomQuote(quoteResponseData){
-      let displayQuote = `
-      <div class="js-quote-result">
-        <p>${quoteResponseData.quote}</p>
+  let url = 'https://api.unsplash.com/photos/random/';
+  let params = {
+      client_id: '9d5c5f49501e8179e3c0fcd3a9e88e155c30a4084ee5141799c20d0db8991db5',
+      query: `${searchTerm}`
+  };
+  $.getJSON(url, params, callback);
+  }
+  
+  function displayUnsplashImages(responseData) {
+      let displayImage = `
+      <div class="js-search-results>
+          <img src=${responseData.urls.regular} alt='a random unsplash image'>
       `;
-      $('js-quote-result').html(displayQuote)
-    }
-    
-    function handleSearchSubmit(ev) {
-      // searchBothApis
-      ev.preventDefault()
-      const searchTerm = $('.js-search-input').val()
-    // 0
-      searchUnsplashImages(searchTerm, displayUnspashImages)
-    // 3
-      randomQuote(displayRandomQuote)
-    }
-    
-    
-    $(function onLoad() {
-     // renderInitialPageContent()
-     $('.container').on('submit', '.js-search-form', handleSearchSubmit)
-    })
+      $('.js-search-results').html(displayImage);
+      console.log(responseData);
+      
+  }
+
+  function getRandomQuote(callback){
+    let url = 'https://talaikis.com/api/quotes/random/';
+    $.getJSON(url, callback)
+  }
+
+  function displayRandomQuote(quoteResponseData){
+    let displayQuote = `
+    <div class="js-quote-result">
+      <p>${quoteResponseData.quote}</p>
+    `;
+    console.log(quoteResponseData);
+    $('.js-quote-result').html(displayQuote);
+  }
+  
+  function handleSearchSubmit() {
+   $('.js-search-form').submit(event => {
+      event.preventDefault();
+      const queryTarget = $(event.currentTarget).find('.js-query');
+      const query = queryTarget.val();
+      searchUnsplashImages(query, displayUnsplashImages);
+      getRandomQuote(displayRandomQuote);
+    });
+
+}
+  
+  
+  
+$(handleSearchSubmit);
