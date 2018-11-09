05b96e67456236758cac79eef099c9880e16a975
jack eichner <jack8191@gmail.com>
2018-11-09 09:37:46 -0800

    Added or fixed core functionality.

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ function searchUnsplashImages(searchTerm, callback) {
   let params = {
       client_id: '9d5c5f49501e8179e3c0fcd3a9e88e155c30a4084ee5141799c20d0db8991db5',
       query: `${searchTerm}`
-  };
-  $.getJSON(url, params, callback);
   }
+  $.ajax({
+    url,
+    method: 'GET',
+    data: params,
+    success: callback,
+    error: errorHandle
+    })
+  }
+
   
   function displayUnsplashImages(responseData) {
       let displayImage = `
       <div class="js-search-results">
           <img src=${responseData.urls.regular} alt='a random unsplash image'>
+          <a href=${responseData.user.links.html}>${responseData.user.username}</a>
+          <a href=${responseData.links.download}>Download Image?</a>
       `;
       $('.js-search-results').html(displayImage);
       console.log(responseData);
@@ -20,19 +29,42 @@ function searchUnsplashImages(searchTerm, callback) {
 
   function getRandomQuote(callback){
     let url = 'https://talaikis.com/api/quotes/random/';
-    $.getJSON(url, callback)
+    $.ajax({
+      url,
+      method: 'GET',
+      success: callback,
+      error: errorHandle
+    })
   }
 
   function displayRandomQuote(quoteResponseData){
     let displayQuote = `
     <div class="js-quote-result">
       <p>${quoteResponseData.quote}</p>
+      <p>—${quoteResponseData.author}</p>
     `;
     console.log(quoteResponseData);
     $('.js-quote-result').html(displayQuote);
   }
+
+  function errorHandle(){
+    let errorMessage = '<p>One of the external API\'s failed to respond. Please try again later.</p>'
+    $('.js-error').html(errorMessage)
+  }
+
+ // function handleHideShowLanding(){
+ //   $('#js-show-hide-landing').click(event => {
+ //     $('#landing').toggleClass( "hidden" )
+ //   });
+ // }
+
+  //function handleHideShowWhyHow(){
+  //  $('#js-show-hide-why-how').click(event => {
+  //    $('.js-why-and-how').toggleClass( "hidden" )
+  //  });
+ //}
   
-  function handleSearchSubmit() {
+  function onLoad() {
    $('.js-search-form').submit(event => {
       event.preventDefault();
       const queryTarget = $(event.currentTarget).find('.js-query');
@@ -40,9 +72,16 @@ function searchUnsplashImages(searchTerm, callback) {
       searchUnsplashImages(query, displayUnsplashImages);
       getRandomQuote(displayRandomQuote);
     });
-
+    $('#js-show-hide-landing').click(event => {
+      $('#landing').toggleClass( "hidden" )
+    });
+    $('#js-show-hide-why-how').click(event => {
+      $('.js-why-and-how').toggleClass( "hidden" )
+    });
 }
   
   
   
-$(handleSearchSubmit);
+$(onLoad);
+//$(handleHideShowLanding);
+//$(handleHideShowWhyHow);
