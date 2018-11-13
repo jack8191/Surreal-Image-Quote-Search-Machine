ffe0ae2c06b2f65e046a8a507aa3dd4e91b6104c
jack eichner <jack8191@gmail.com>
2018-11-12 23:47:42 -0800

    added readme text and brief comments to css and js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# api-capstone
+Surreal Image/Quote Search Machine
+
+Deployed at: https://jack8191.github.io/Surreal-Image-Quote-Search-Machine/
+
+Screenshots:
+    Landing Page
+<img src="https://drive.google.com/file/d/135lKJQ5xR9qxuSqcrDNTQTLb2sRZposJ/view?usp=sharing">
+
+Summary of Function
+    This app takes a search term from the user and returns a random image based on the search term from Unsplash paired with a random quote
+    from the random quotes API by Tadas Talaikis. 
+
+Technology Used
+    HTML, CSS, JavaScript, and jQuery
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//For accessing the Unsplash API
 function searchUnsplashImages(searchTerm, callback) {
   let url = 'https://api.unsplash.com/photos/random/';
   let params = {
@@ -14,7 +14,7 @@ function searchUnsplashImages(searchTerm, callback) {
     })
   }
 
-  
+  //Rendering those results in the DOM
   function displayUnsplashImages(responseData) {
       let displayImage = `
       <div class="js-search-results">
@@ -26,6 +26,7 @@ function searchUnsplashImages(searchTerm, callback) {
       $('.js-search-results').html(displayImage);      
   }
 
+  //For accessing the random quote API
   function getRandomQuote(callback){
     let url = 'https://talaikis.com/api/quotes/random/';
     $.ajax({
@@ -36,6 +37,7 @@ function searchUnsplashImages(searchTerm, callback) {
     })
   }
 
+  //For rendering the random quote in the DOM
   function displayRandomQuote(quoteResponseData){
     let displayQuote = `
     <div class="js-quote-result">
@@ -45,11 +47,13 @@ function searchUnsplashImages(searchTerm, callback) {
     $('.js-quote-result').html(displayQuote);
   }
 
+  //Called when one of the API's fails to return useable data
   function errorHandle(){
     let errorMessage = '<p>One of the external API\'s failed to respond. Please try again later.</p>'
     $('.js-error').html(errorMessage)
   }
   
+  //Contains event listeners for all buttons including search submit and showing and hiding explanation text
   function onLoad() {
    $('.js-search-form').submit(event => {
       event.preventDefault();
